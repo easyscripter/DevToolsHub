b04169b3afb1eb6900f80123ade5cd26e7bb22b6
easyscripter <deniscs49@gmail.com>
2025-06-29 14:06:37 +0300

    fix: add editing description

--- app/workspaces/[workspaceId]/page.tsx
+++ app/workspaces/[workspaceId]/page.tsx
@@ -13,11 +13,15 @@ type WorkspaceParams = {
 export default function Workspace() {
 	const { workspaceId } = useParams<WorkspaceParams>();
 	const { workspaces, updateWorkspace } = useWorkspacesStore();
-	const [isEditing, setIsEditing] = useState(false);
+	const [isEditingName, setIsEditingName] = useState(false);
+	const [isEditingDescription, setIsEditingDescription] = useState(false);
 
 	const workspace = workspaces.find(workspace => workspace.id === workspaceId);
 
 	const [workspaceName, setWorkspaceName] = useState(workspace?.name);
+	const [workspaceDescription, setWorkspaceDescription] = useState(
+		workspace?.description
+	);
 
 	if (!workspace) {
 		return <div>Workspace not found</div>;
@@ -28,33 +32,53 @@ export default function Workspace() {
 			...workspace,
 			name: workspaceName || '',
 		});
-		setIsEditing(false);
+		setIsEditingName(false);
 	};
 
 	const handleCancelEdit = () => {
 		setWorkspaceName(workspace.name);
-		setIsEditing(false);
+		setIsEditingName(false);
 	};
 
 	const handleStartEdit = () => {
 		setWorkspaceName(workspace.name);
-		setIsEditing(true);
+		setIsEditingName(true);
 	};
 
-	const isValidation = workspaceName && workspaceName.length > 0;
+	const handleUpdateWorkspaceDescription = () => {
+		updateWorkspace({
+			...workspace,
+			description: workspaceDescription || '',
+		});
+		setIsEditingDescription(false);
+	};
+
+	const handleCancelEditDescription = () => {
+		setWorkspaceDescription(workspace.description);
+		setIsEditingDescription(false);
+	};
+
+	const handleStartEditDescription = () => {
+		setWorkspaceDescription(workspace.description);
+		setIsEditingDescription(true);
+	};
+
+	const isValidationName = workspaceName && workspaceName.length > 0;
+	const isValidationDescription =
+		workspaceDescription && workspaceDescription.length > 0;
 
 	return (
 		<div className='flex flex-col gap-6'>
 			<div className='flex flex-col gap-2 mt-4'>
 				<div className='flex max-w-1/4 items-center gap-4'>
-					{isEditing ? (
+					{isEditingName ? (
 						<>
 							<Input
 								type='text'
 								value={workspaceName}
 								onChange={e => setWorkspaceName(e.target.value)}
 							/>
-							{isValidation && (
+							{isValidationName && (
 								<CheckIcon
 									className='w-5 h-5 cursor-pointer'
 									onClick={handleUpdateWorkspaceName}
@@ -68,10 +92,43 @@ export default function Workspace() {
 					) : (
 						<>
 							<h1 className='text-3xl font-bold'>{workspace.name}</h1>
-							<EditIcon
-								className='w-5 h-5 cursor-pointer mt-2'
-								onClick={handleStartEdit}
+							{!isEditingDescription && (
+								<EditIcon
+									className='w-5 h-5 cursor-pointer mt-2'
+									onClick={handleStartEdit}
+								/>
+							)}
+						</>
+					)}
+				</div>
+				<div className='flex max-w-1/4 items-center gap-4'>
+					{isEditingDescription ? (
+						<>
+							<Input
+								type='text'
+								value={workspaceDescription}
+								onChange={e => setWorkspaceDescription(e.target.value)}
 							/>
+							{isValidationDescription && (
+								<CheckIcon
+									className='w-5 h-5 cursor-pointer'
+									onClick={handleUpdateWorkspaceDescription}
+								/>
+							)}
+							<XIcon
+								className='w-5 h-5 cursor-pointer'
+								onClick={handleCancelEditDescription}
+							/>
+						</>
+					) : (
+						<>
+							<p className='text-muted-foreground'>{workspace.description}</p>
+							{!isEditingName && (
+								<EditIcon
+									className='w-5 h-5 cursor-pointer'
+									onClick={handleStartEditDescription}
+								/>
+							)}
 						</>
 					)}
 				</div>
